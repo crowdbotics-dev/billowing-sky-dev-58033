05b746505dbe1f4f5150d40019b5e288068eb4ea
Crowdbotics <engineering@crowdbotics.com>
2023-01-19 07:38:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billowing-sky-dev-58033",
+  url: "https://billowing-sky-dev-58033.botics.co",
+  api: "https://billowing-sky-dev-58033.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
